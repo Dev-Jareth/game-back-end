40c6ab3f97558f301a39901e7f1cfe1029dd81ba
Jareth <seraphimjester@gmail.com>
2018-02-16 16:02:43 +0000

    Add secure api - socket request authenticated frontend can request socket message types

--- api/secure.js
+++ api/secure.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import messageTypes from '../socket/messageType.json';
 const router = Router()
 router.get('/test', (req, res) => {
   res.send("It worked");
+})
+router.get('/socket-message-types', (req, res) => {
+  res.send(JSON.stringify(messageTypes));
 })
 
 export default router;
