e35a41910500c312eeef5724596837d20f58da73
Jareth <seraphimjester@gmail.com>
2018-03-16 15:05:23 +0000

    Add ship schema functionality ship schema given date_created and date_updated along with pre-save hook

--- data/models/ship.js
+++ data/models/ship.js
@@ -1,14 +1,23 @@
 import Mongoose from 'mongoose';
 
 export const schema = new Mongoose.Schema({
-    name: { type: String, required: true, default: "Sabre" },
-    hullCapacity: { type: Number, default: 100 },
-    hullStrength: { type: Number, default: 100 },
-    modelLocation: String,
-    defensiveSlotsCount: { type: Number, required: true, default: 1 },
-    offensiveSlotsCount: { type: Number, required: true, default: 1 },
-    defensiveSlots: Array,
-    offensiveSlots: Array
+  created_at: Date,
+  updated_at: Date,
+  name: { type: String, required: true, default: "Sabre" },
+  hullCapacity: { type: Number, default: 100 },
+  hullStrength: { type: Number, default: 100 },
+  modelLocation: String,
+  defensiveSlotsCount: { type: Number, required: true, default: 1 },
+  offensiveSlotsCount: { type: Number, required: true, default: 1 },
+  defensiveSlots: Array,
+  offensiveSlots: Array
+})
+
+schema.pre('save', function(next) {
+  let date = new Date()
+  this.updated_at = date;
+  if (!this.created_at) this.created_at = date;
+  next()
 })
 
 export const Ship = Mongoose.model('Ship', schema)
