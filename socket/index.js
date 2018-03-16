f6495cc2831348d39795a78be942d3ed1730ef29
Jareth <seraphimjester@gmail.com>
2018-03-16 16:30:52 +0000

    WS sendMap function messageType for sendMap and requestMap added socket responds to requestMap with planets from DB (TODO: include other models when added)

--- socket/index.js
+++ socket/index.js
@@ -7,12 +7,12 @@ import DB from '../data';
 const shouldLog = true;
 
 //## Functions
-const log = (...msg)=>(!shouldLog)?void(0):console.log(`||WS|| `,...msg)
-const onmessage = function (name, callback) {
+const log = (...msg) => (!shouldLog) ? void(0) : console.log(`||WS|| `, ...msg)
+const onmessage = function(name, callback) {
   if (!this.messagelisteners) this.messagelisteners = {};
   this.messagelisteners[name] = callback;
 }
-const sendMessage = function (name, payload = false) {
+const sendMessage = function(name, payload = false) {
   log(`Sending ${name} message to client`)
   try {
     if (this.readyState === WebSocket.OPEN)
@@ -24,7 +24,7 @@ const sendMessage = function (name, payload = false) {
     log(`Failed to send ""${name}"" message because "${e.message}"`)
   }
 }
-const handlemessage = function (message) {
+const handlemessage = function(message) {
   let msg = JSON.parse(message);
   let type = Object.keys(msg)[0]
   let payload = msg[type];
@@ -41,33 +41,39 @@ const onConnect = ws => {
   ws.handlemessage = handlemessage.bind(ws)
   ws.on('message', ws.handlemessage);
   ws.on('close', () => log("Closing connection"))
-  ws.on('error', error => error.code !== 'ECONNRESET' ? log("ERROR", error) : void (0))
+  ws.on('error', error => error.code !== 'ECONNRESET' ? log("ERROR", error) : void(0))
   //##
 
   ws.sendMessage(MESSAGE.server.requestAuth)
   ws.onmessage(MESSAGE.client.sendAuth, validateAuth.bind(ws))
   ws.onmessage(MESSAGE.client.requestPlayerData, sendPlayerData.bind(ws))
+  ws.onmessage(MESSAGE.client.requestMap, sendMap.bind(ws))
 
 }
-const validateAuth = function(token){
-  try {
-      let userId = jwt.verify(token, process.env.SECRET_KEY)
-      this.authorised = {id:userId};
-      this.sendMessage(MESSAGE.server.acceptAuth)
-    } catch (e) {
-      this.authorised = false;
-      this.sendMessage(MESSAGE.server.refuseAuth)
-    }
+const sendMap = async function() {
+  let planets = await DB.getAllPlanets()
+  let response = { planets }
+  this.sendMessage(MESSAGE.server.sendMap, response)
 }
-const sendPlayerData = async function(){
-  if(!this.authorised) return this.sendMessage(MESSAGE.server.unauthorised);
+const validateAuth = function(token) {
+  try {
+    let userId = jwt.verify(token, process.env.SECRET_KEY)
+    this.authorised = { id: userId };
+    this.sendMessage(MESSAGE.server.acceptAuth)
+  } catch (e) {
+    this.authorised = false;
+    this.sendMessage(MESSAGE.server.refuseAuth)
+  }
+}
+const sendPlayerData = async function() {
+  if (!this.authorised) return this.sendMessage(MESSAGE.server.unauthorised);
   let user = await DB.getUserById(JSON.parse(this.authorised.id))
   user = JSON.parse(JSON.stringify(user))
   delete user.password
   delete user._id
   delete user.created_at
   delete user.updated_at
-  this.sendMessage(MESSAGE.server.sendPlayerData,user)
+  this.sendMessage(MESSAGE.server.sendPlayerData, user)
 }
 
 export default onConnect;
